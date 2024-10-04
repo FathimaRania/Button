889653901ea33100c9c03353dbd8a3d000585441
FathimaRania <162035187+FathimaRania@users.noreply.github.com>
2024-10-04 14:34:31 +0530

    Create j.js

---
+++ j.js
@@ -0,0 +1,28 @@
+function redcolor(){
+    // document.querySelector('body').style.backgroundColor="red";
+    let one=document.querySelector('body');
+    // one.style.backgroundColor='red';
+    one.classList.add('one')
+}
+function bluecolor(){
+    // document.querySelector('#container').style.backgroundColor="green";
+    // document.querySelector('body').style.backgroundColor="red";
+    let one=document.querySelector('body');
+    // one.style.backgroundColor='red';
+    one.classList.add('two')
+}
+function greencolor(){
+    // document.querySelector('#container').style.backgroundColor="blue";
+    // document.querySelector('body').style.backgroundColor="red";
+    let one=document.querySelector('body');
+    // one.style.backgroundColor='red';
+    one.classList.add('three')
+}
+function resetcolor(){
+    // document.querySelector('#container').style.backgroundColor="bisque";
+    // document.querySelector('body').style.backgroundColor="red";
+    let one=document.querySelector('body');
+    // one.style.backgroundColor='red';
+    one.classList.add('normal')
+}
+
